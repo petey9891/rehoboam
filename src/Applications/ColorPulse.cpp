7383b0d74f072870b176b30807cc62357a2204ff
Andrew Peterson <16apeterson@gmail.com>
2021-03-21 14:26:39 -0500

    reverted back to original code

--- src/Applications/ColorPulse.cpp
+++ src/Applications/ColorPulse.cpp
@@ -6,26 +6,24 @@ ColorPulse::ColorPulse(rgb_matrix::FrameCanvas *c): Runnable(c) {
 }
 
 void ColorPulse::run() {
-    printf("Running\n");
-    // usleep(5 * 1000);
-    // this->continuum += 1;
-    // this->continuum %= 3 * 255;
+    usleep(5 * 1000);
+    this->continuum += 1;
+    this->continuum %= 3 * 255;
 
-    // int r = 0, g = 0, b = 0;
+    int r = 0, g = 0, b = 0;
 
-    // if (this->continuum <= 255) {
-    //     int c = this->continuum;
-    //     b = 255 - c;
-    //     r = c;
-    // } else if (this->continuum > 255 && this->continuum <= 511) {
-    //     int c = this->continuum - 256;
-    //     r = 255 - c;
-    //     g = c;
-    // } else {
-    //     int c = this->continuum - 512;
-    //     g = 255 - c;
-    //     b = c;
-    // }
-    // this->canvas->Fill(r, g, b);
-    this->canvas->Fill(255, 255, 255);
+    if (this->continuum <= 255) {
+        int c = this->continuum;
+        b = 255 - c;
+        r = c;
+    } else if (this->continuum > 255 && this->continuum <= 511) {
+        int c = this->continuum - 256;
+        r = 255 - c;
+        g = c;
+    } else {
+        int c = this->continuum - 512;
+        g = 255 - c;
+        b = c;
+    }
+    this->canvas->Fill(r, g, b);
 };
