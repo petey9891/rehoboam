c639001d42cbc36a6694da54ab798f18cbc0aa21
Andrew Peterson <16apeterson@gmail.com>
2021-03-21 19:11:21 -0500

    reverted back to GL_RGB

--- src/Applications/Loading.cpp
+++ src/Applications/Loading.cpp
@@ -62,8 +62,8 @@ void Loading::run() {
     this->renderer.drawArrays(this->shader);
     printf("finished drawing\n");
 
-    // GLCall(glReadPixels(0, 0, PANEL_WIDTH, PANEL_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, this->buffer));
-    GLCall(glReadPixels(0, 0, PANEL_WIDTH, PANEL_HEIGHT, GL_RGBA, GL_UNSIGNED_BYTE, this->buffer));
+    GLCall(glReadPixels(0, 0, PANEL_WIDTH, PANEL_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, this->buffer));
+    // GLCall(glReadPixels(0, 0, PANEL_WIDTH, PANEL_HEIGHT, GL_RGBA, GL_UNSIGNED_BYTE, this->buffer));
 
 
     printf("about to set pixels\n");
