1be6804df1816da3c5dc9aecdf193a712c74fe5e
Andrew Peterson <16apeterson@gmail.com>
2021-04-07 18:23:59 -0500

    chore: disabled server code for now

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,9 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    SocketServer server(60000);
-    server.Start();
-    server.HandleRequests();
+    // SocketServer server(60000);
+    // server.Start();
+    // server.HandleRequests();
 
     // Clear the whole screen (front buffer)
 	glClearColor(0.0f, 0.0f, 0.4f, 1.0f);
@@ -35,12 +35,12 @@ int main(int argc, char* argv[]) {
 
     // Create shaders
     Shader loadingShader("/home/pi/rehoboam/shaders/loading.shader");
-    // Shader rehoboamShader("/home/pi/rehoboam/shaders/rehoboam.shader");
+    Shader rehoboamShader("/home/pi/rehoboam/shaders/rehoboam.shader");
     
     // Create applications
     // Loading loading(loadingShader, canvas);
     Loading* loading = new Loading(loadingShader, matrix, canvas);
-    // Rehoboam rehoboam(rehoboamShader, canvas);
+    Rehoboam* rehoboam = new Rehoboam(rehoboamShader, matrix, canvas);
     ColorPulse* pulse = new ColorPulse(matrix, canvas);
 
     // Bind the loading shader for the loading sequence
@@ -53,19 +53,19 @@ int main(int argc, char* argv[]) {
     program->setInitialState();
 
     bool power = true;
-    Command cmd;
+    // Command cmd;
 
     while (true) {
-        if (server.hasCommands()) {
-            Command nextCmd = server.getNextCommand();
-            cmd = nextCmd;
+        // if (server.hasCommands()) {
+        //     Command nextCmd = server.getNextCommand();
+        //     cmd = nextCmd;
 
-            if (cmd == DisplayOn) {
-                power = true;
-            } else if (cmd == DisplayOff) {
-                power = false;
-            }
-        }
+        //     if (cmd == DisplayOn) {
+        //         power = true;
+        //     } else if (cmd == DisplayOff) {
+        //         power = false;
+        //     }
+        // }
         
         if (power) {
             program->run();
@@ -87,7 +87,7 @@ int main(int argc, char* argv[]) {
     delete pulse;
     delete loading;
 
-    server.Stop();
+    // server.Stop();
     window.destroy();
 
     return 0;
