bbec09b59b9c0b9303b00079cbc8bd966974eade
Andrew Peterson <16apeterson@gmail.com>
2021-05-03 19:26:05 -0500

    chore: updated pwm lsb nanoseconds

--- include/RGBMatrixConfig.h
+++ include/RGBMatrixConfig.h
@@ -15,13 +15,13 @@ struct RGBMatrixConfig {
         defaults.hardware_mapping = "adafruit-hat-pwm";
         defaults.led_rgb_sequence = "BGR";
         // defaults.pwm_lsb_nanoseconds = 50;
-        defaults.pwm_lsb_nanoseconds = 200;
+        defaults.pwm_lsb_nanoseconds = 180;
         // defaults.panel_type = "FM6126A";
         defaults.rows = 64;
         defaults.cols = 192;
         defaults.chain_length = 1;
         // defaults.brightness = 100; // 60 is a good brightness for downtime
-        // defaults.brightness = 80;
+        defaults.brightness = 80;
         // defaults.brightness = 20;
 
         defaults.show_refresh_rate = true;
