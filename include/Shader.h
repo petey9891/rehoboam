ab50ee9ce31c7e1831f1eb29f06050597b464136
Andrew Peterson <16apeterson@gmail.com>
2021-05-19 22:23:05 -0500

    fixed types

--- include/Shader.h
+++ include/Shader.h
@@ -39,7 +39,7 @@ class Shader {
         // Shader generation
         std::string parseShader(const std::filesystem::path path);
         std::vector<std::filesystem::path> aggregateShaders(const ShaderType type);
-        unsigned int compileShader(unsigned int type, const std::vector<std::string> sourceFiles);
+        unsigned int compileShader(unsigned int type, const std::vector<std::filesystem::path> sourceFiles);
         unsigned int createShaders();
 
 };
