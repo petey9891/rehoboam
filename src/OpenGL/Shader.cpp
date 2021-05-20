561ac6ffbb97a7426f7c35b6874ab51aff6d545c
Andrew Peterson <16apeterson@gmail.com>
2021-05-19 19:59:29 -0500

    chore: added back missing lines

--- src/OpenGL/Shader.cpp
+++ src/OpenGL/Shader.cpp
@@ -168,26 +168,30 @@ float phi;
 float threadf = 0.0;
 
 void main() {
-    vec2 coords = backgroundCoord.xy*0.5;
+vec2 coords = backgroundCoord.xy*0.5;
     float radius = 0.25;
 
-    float phi = (atan(coords.y, coords.x)+3.1415926538)/3.1415926538*float(CORES)*0.5;    
-    vec2 i = background;    
-    float c = 1.0;    
+    float phi = (atan(coords.y, coords.x)+3.1415926538)/3.1415926538*float(CORES)*0.5;
+    vec2 background = backgroundCoord.xy * 0.5 * 10.0 - vec2(19.0);
+    vec2 i = background;
+    float c = 1.0;
+    float inten = 0.05;
 
     for (int n = 0; n < 8; n++) {
-        float t = time * (0.7 - (0.2 / float(n+1)));        
+        float t = time * (0.7 - (0.2 / float(n+1)));
+        i = background + vec2(cos(t - i.x) + sin(t + i.y), sin(t - i.y) + cos(t + i.x));
         c += 1.0 / length(vec2(background.x / (2.0 * sin(i.x + t) / inten), background.y / (cos(i.y + t) / inten)));
     }
 
     c /= 8.0;
     c = 1.5 - sqrt(pow(c, 2.0));
-        
+
+    bColor.g = clamp(coords.x, 0.0, 1.0);
     bColor = smoothstep(T2, T1, temperature)*bColor + smoothstep(T1, T2, temperature)*smoothstep(T3, T2, temperature)*bColorWarm + smoothstep(T2, T3, temperature)*bColorHot;
-    
+
     rColor = smoothstep(50.0, 0.0, threadf)*rColor + smoothstep(0.0, 50.0, threadf)*smoothstep(100.0, 50.0, threadf)*rColorWarm + smoothstep(50.0, 100.0, threadf)*rColorHot;
     // rColor *= circle(coords, radius, 0.01);
-        
+
     vec3 outcolor = bColor * c * c * c * c + rColor;
 
     float coreIndex = 0.0;
