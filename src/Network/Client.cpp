ea051cf787d384a55c6a18639a6f78883929e8e1
Andrew Peterson <16apeterson@gmail.com>
2021-05-31 20:03:12 -0500

    chore: disabled everything else

--- src/Network/Client.cpp
+++ src/Network/Client.cpp
@@ -25,24 +25,24 @@ void Client::OnMessageRecieved(Message<MessageType>& msg) {
         case CubeDisplayOnOff:
             if (!this->power) {
                 this->power = true;
-                this->commands.push_front({ DisplayOn, 0, nullptr});
+                // this->commands.push_front({ DisplayOn, 0, nullptr});
             } else {
                 this->power = false;
-                this->commands.push_front({ DisplayOff, 0, nullptr});
+                // this->commands.push_front({ DisplayOff, 0, nullptr});
             }
             break;
         case CubeBrightness: {
-            uint8_t value[1];
-            msg >> value;
+            // uint8_t value[1];
+            // msg >> value;
 
-            this->commands.push_front({ Brightness, 1, value });
+            // this->commands.push_front({ Brightness, 1, value });
             break;
         }
         case CubePulse:
-            this->commands.push_front({ ColorPulseMode, 0, nullptr });
+            // this->commands.push_front({ ColorPulseMode, 0, nullptr });
             break;
         case CubeRehoboam:
-            this->commands.push_front({ RehoboamMode, 0, nullptr });
+            // this->commands.push_front({ RehoboamMode, 0, nullptr });
             break;
         case SetSolidColor: {
             // std::vector<uint8_t> rgb;
