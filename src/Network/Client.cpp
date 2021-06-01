7c6e8c44bda6cddb5d4ecab9f40bbda5a5df8463
Andrew Peterson <16apeterson@gmail.com>
2021-05-31 22:24:26 -0500

    chore: removed logging

--- src/Network/Client.cpp
+++ src/Network/Client.cpp
@@ -16,7 +16,6 @@ bool Client::hasCommands() {
 }
 
 void Client::OnMessageRecieved(Message<MessageType>& msg) {
-    printf("new command incoming: %d\n", msg.header.id);
     switch (msg.header.id) {
         case ServerPing:
             break;
