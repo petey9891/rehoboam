ff2643d69c990c0c93f0c7e2c2da8d252be07f89
Andrew Peterson <16apeterson@gmail.com>
2021-05-31 21:32:46 -0500

    chore: added logging

--- src/Applications/SolidColor.cpp
+++ src/Applications/SolidColor.cpp
@@ -8,15 +8,25 @@ SolidColor::SolidColor(rgb_matrix::RGBMatrix* m, rgb_matrix::FrameCanvas* c): Ru
 void SolidColor::setCommand(Command cmd) {
     if (cmd.type == StaticColor) {
         if (cmd.data.size() == 3) {
-            this->color = { cmd.data[0], cmd.data[1], cmd.data[2] };
+            const float r = cmd.data[0];
+            const float g = cmd.data[1];
+            const float b = cmd.data[2];
+
+            this->color = { r, g, b };
             this->canvas->Fill(this->color.r, this->color.g, this->color.b);
         }
     } else if (cmd.type == Brightness) {
         const uint8_t brightness = cmd.data[0] / 100.0f;
+        
+        printf("brightness: %f\n", brightness);
 
         this->color.r *= brightness;
         this->color.b *= brightness;
         this->color.g *= brightness;
+
+        printf("r: %f\n", this->color.r);
+        printf("g: %f\n", this->color.g);
+        printf("b: %f\n", this->color.b);
 
         this->canvas->Fill(this->color.r, this->color.g, this->color.b);
     }
