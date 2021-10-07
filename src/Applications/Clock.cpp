e52c0a0006cba696fff3ac36fead0b0948bd0d6e
Andrew Peterson <16apeterson@gmail.com>
2021-10-07 08:47:18 -0500

    chore: updated height

--- src/Applications/Clock.cpp
+++ src/Applications/Clock.cpp
@@ -27,7 +27,7 @@ void Clock::run() {
     strftime(buffer, sizeof(buffer), format.c_str(), &tm);
 
     rgb_matrix::DrawText(this->canvas, this->font, 0, 0 + this->font.baseline(), rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
-    rgb_matrix::DrawText(this->canvas, this->font, 0, this->font.height() + this->font.baseline(), rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
+    rgb_matrix::DrawText(this->canvas, this->font, 0, 128 + this->font.baseline(), rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
     
     clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &nextTime, nullptr);
     
