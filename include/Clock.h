de97b136eca784e94f2f8e51b07327687a9030bb
Andrew Peterson <16apeterson@gmail.com>
2021-10-07 08:27:21 -0500

    feat: added text drawing

--- include/Clock.h
+++ include/Clock.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Runnable.h>
+#include <rpi-led-matrix/graphics.h>
 
 class Clock: public Runnable {
 public:
@@ -9,4 +10,11 @@ public:
 
     void run() override;
     void setCommand(Command cmd) override;
+    
+private:
+    char buffer[256];
+    struct timespec nextTime;
+    struct tm tm;
+    rgb_matrix::Font font;
+    std::string format = "%H:%M";
 };
--- src/Applications/Clock.cpp
+++ src/Applications/Clock.cpp
@@ -1,7 +1,17 @@
 #include <Clock.h>
+#include <time.h>
+
 
 Clock::Clock(rgb_matrix::RGBMatrix* m, rgb_matrix::FrameCanvas* c): Runnable(m, c) {
     printf(">>> <Clock> Initializing Clock application\n");
+    
+    this->nextTime.tv_sec = time(nullptr);
+    this->nextTime.tv_nsec = 0;
+
+    if (!this->font.LoadFont("../../resources/fonts/8x13.bdf")) {
+        printf(">>> <Clock> Error: Unable to load font\n");
+    }
+    
     printf(">>> <Clock> Initialized Clock application\n");
 }
 
@@ -10,6 +20,15 @@ void Clock::setCommand(Command cmd) {
 };
 
 void Clock::run() {
-    this->canvas->Fill(50, 102, 168);
+    this->canvas->Fill(0, 0, 0);
+    localtime_r(&nextTime.tv_sec, &tm);
+
+    strftime(buffer, sizeof(buffer), format.c_str(), &tm);
+
+    rgb_matrix::DrawText(this->canvas, this->font, 0, 0 + this->font.baseline(), rgb_matrix::Color(), nullptr, buffer, 0);
+    
+    clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &nextTime, nullptr);
+    
     this->canvas = this->matrix->SwapOnVSync(this->canvas);
+    nextTime.tv_sec += 1;
 };
