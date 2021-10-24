c770da21f37271369f8062ffa06d02fb236be4c7
Andrew Peterson <16apeterson@gmail.com>
2021-10-24 17:13:00 -0500

    another one

--- src/Applications/Clock.cpp
+++ src/Applications/Clock.cpp
@@ -58,7 +58,8 @@ void Clock::run() {
     rgb_matrix::DrawText(this->canvas, this->font, 0, this->font.baseline() * 13, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
     rgb_matrix::DrawText(this->canvas, this->font, 0, this->font.baseline() * 14, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
     rgb_matrix::DrawText(this->canvas, this->font, 0, this->font.baseline() * 15, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
-    // rgb_matrix::DrawText(this->canvas, this->font, 64, this->font.baseline() * 2, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
+    
+    rgb_matrix::DrawText(this->canvas, this->font, 64, this->font.baseline() * 2, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
     // rgb_matrix::DrawText(this->canvas, this->font, 64, this->font.baseline() * 3, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
     // rgb_matrix::DrawText(this->canvas, this->font, 64, this->font.baseline() * 4, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
     // rgb_matrix::DrawText(this->canvas, this->font, 64, this->font.baseline() * 5, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
