6004418126660173bc363e83314d984af70e85fc
Andrew Peterson <16apeterson@gmail.com>
2021-10-24 18:40:50 -0500

    changed to matrix

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char* argv[]) {
     window.createEGLWindow();
 
     RGBMatrixConfig config;
-    RGBMatrix* matrix = rgb_matrix::CreateFromFlags(&argc, &argv, &config.defaults, &config.runtime);
+    RGBMatrix* matrix = rgb_matrix::CreateMatrixFromFlags(&argc, &argv, &config.defaults, &config.runtime);
     FrameCanvas* canvas = matrix->CreateFrameCanvas();
     if (matrix == nullptr) {
         fprintf(stderr, "Error! Unable to create matrix!\n");
