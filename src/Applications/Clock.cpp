8c10a6df2322ae1776b03c8bfcc76c11aac0eb05
Andrew Peterson <16apeterson@gmail.com>
2021-10-24 17:02:31 -0500

    chore: changed distance

--- src/Applications/Clock.cpp
+++ src/Applications/Clock.cpp
@@ -44,7 +44,8 @@ void Clock::run() {
     rgb_matrix::DrawText(this->canvas, this->font, 0, this->font.baseline() * 4, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
     rgb_matrix::DrawText(this->canvas, this->font, 0, this->font.baseline() * 5, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
     rgb_matrix::DrawText(this->canvas, this->font, 0, this->font.baseline() * 6, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
-    rgb_matrix::DrawText(this->canvas, this->font, 0, this->font.baseline() * 7, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
+
+    rgb_matrix::DrawText(this->canvas, this->font, 50, this->font.baseline(), rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
 
     // rgb_matrix::DrawText(this->canvas, this->font, this->canvas->width()/2, this->canvas->height()/2 + this->font.baseline(), rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
 
