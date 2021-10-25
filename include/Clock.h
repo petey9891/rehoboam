3a60bc921de21ce31c41612baf966a64c21a1803
Andrew Peterson <16apeterson@gmail.com>
2021-10-24 21:51:23 -0500

    chore: added new buffers

--- include/Clock.h
+++ include/Clock.h
@@ -14,11 +14,13 @@ public:
 
 private:
     char buffer[256];
-    char dateBuffer[256];
+    char weekday[256];
+    char month[256];
+    char date[256];
     struct timespec nextTime;
     struct tm tm;
     rgb_matrix::Font font;
     int heightOffset = 3;
     std::string format = "%I:%M:%S";
-    std::string dateFormat = "%a, %b %d";
+    // std::string dateFormat = "%a, %b %d";
 };
--- src/Applications/Clock.cpp
+++ src/Applications/Clock.cpp
@@ -26,13 +26,14 @@ void Clock::run() {
     localtime_r(&nextTime.tv_sec, &tm);
 
     strftime(buffer, sizeof(buffer), format.c_str(), &tm);
-    strftime(dateBuffer, sizeof(dateBuffer), dateFormat.c_str(), &tm);
+    strftime(weekday, sizeof(weekday), "%A", &tm);
+    strftime(month, sizeof(month), "%B %d", &tm);
     
-    rgb_matrix::DrawText(this->canvas, this->font, 0, this->heightOffset + this->font.baseline(), rgb_matrix::Color(255, 255, 255), nullptr, dateBuffer, 0);
-    rgb_matrix::DrawText(this->canvas, this->font, 0, this->heightOffset + this->font.baseline() * 2, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
-    rgb_matrix::DrawText(this->canvas, this->font, 0, this->heightOffset + this->font.baseline() * 3, rgb_matrix::Color(255, 255, 255), nullptr, dateBuffer, 0);
+    rgb_matrix::DrawText(this->canvas, this->font, 0, this->heightOffset + this->font.baseline(), rgb_matrix::Color(255, 255, 255), nullptr, weekday, 0);
+    rgb_matrix::DrawText(this->canvas, this->font, 0, this->heightOffset + this->font.baseline() * 2, rgb_matrix::Color(255, 255, 255), nullptr, month, 0);
     rgb_matrix::DrawText(this->canvas, this->font, 0, this->heightOffset + this->font.baseline() * 4, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
-    rgb_matrix::DrawText(this->canvas, this->font, 0, this->heightOffset + this->font.baseline() * 5, rgb_matrix::Color(255, 255, 255), nullptr, dateBuffer, 0);
+    // rgb_matrix::DrawText(this->canvas, this->font, 0, this->heightOffset + this->font.baseline() * 3, rgb_matrix::Color(255, 255, 255), nullptr, date, 0);
+    // rgb_matrix::DrawText(this->canvas, this->font, 0, this->heightOffset + this->font.baseline() * 5, rgb_matrix::Color(255, 255, 255), nullptr, dateBuffer, 0);
 
     rgb_matrix::DrawText(this->canvas, this->font, 64, this->heightOffset + this->font.baseline(), rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
     rgb_matrix::DrawText(this->canvas, this->font, 64, this->heightOffset + this->font.baseline() * 2, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
