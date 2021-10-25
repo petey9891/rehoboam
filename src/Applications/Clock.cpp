fcba331afc9d242744d508cdcdf3e783033de027
Andrew Peterson <16apeterson@gmail.com>
2021-10-24 20:19:20 -0500

    chore: added all closks

--- src/Applications/Clock.cpp
+++ src/Applications/Clock.cpp
@@ -43,8 +43,22 @@ void Clock::run() {
     rgb_matrix::DrawText(this->canvas, this->font, 0, this->font.baseline() * 5, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
     rgb_matrix::DrawText(this->canvas, this->font, 0, this->font.baseline() * 6, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
 
+    rgb_matrix::DrawText(this->canvas, this->font, 64, this->font.baseline(), rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
+    rgb_matrix::DrawText(this->canvas, this->font, 64, this->font.baseline() * 2, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
+    rgb_matrix::DrawText(this->canvas, this->font, 64, this->font.baseline() * 3, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
+    rgb_matrix::DrawText(this->canvas, this->font, 64, this->font.baseline() * 4, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
+    rgb_matrix::DrawText(this->canvas, this->font, 64, this->font.baseline() * 5, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
+    rgb_matrix::DrawText(this->canvas, this->font, 64, this->font.baseline() * 6, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
 
-    rgb_matrix::DrawText(this->canvas, this->font, 0, this->font.baseline(), rgb_matrix::Color(255, 255, 255), nullptr, buffer1, 0);
+
+    rgb_matrix::DrawText(this->canvas, this->font, 128, this->font.baseline(), rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
+    rgb_matrix::DrawText(this->canvas, this->font, 128, this->font.baseline() * 2, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
+    rgb_matrix::DrawText(this->canvas, this->font, 128, this->font.baseline() * 3, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
+    rgb_matrix::DrawText(this->canvas, this->font, 128, this->font.baseline() * 4, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
+    rgb_matrix::DrawText(this->canvas, this->font, 128, this->font.baseline() * 5, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
+    rgb_matrix::DrawText(this->canvas, this->font, 128, this->font.baseline() * 6, rgb_matrix::Color(255, 255, 255), nullptr, buffer, 0);
+
+
     // rgb_matrix::DrawText(this->canvas, this->font, 0, 64 + this->font.baseline(), rgb_matrix::Color(255, 255, 255), nullptr, buffer2, 0);
     // rgb_matrix::DrawText(this->canvas, this->font, 0, 128 + this->font.baseline(), rgb_matrix::Color(255, 255, 255), nullptr, buffer3, 0);   
     // rgb_matrix::DrawText(this->canvas, this->font, 64, this->font.baseline(), rgb_matrix::Color(255, 255, 255), nullptr, buffer2, 0);
