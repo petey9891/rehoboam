a56cb1595e29ee09c028797af774852ef405f917
Andrew Peterson <16apeterson@gmail.com>
2021-12-15 20:25:35 -0600

    chore: added file reader

--- src/OpenGL/Shader.cpp
+++ src/OpenGL/Shader.cpp
@@ -9,8 +9,25 @@
 
 namespace fs = std::filesystem;
 
+// Reads a text file and outputs a string with everything in the text file
+std::string get_file_contents(const char* filename)
+{
+	std::ifstream in(filename, std::ios::binary);
+	if (in)
+	{
+		std::string contents;
+		in.seekg(0, std::ios::end);
+		contents.resize(in.tellg());
+		in.seekg(0, std::ios::beg);
+		in.read(&contents[0], contents.size());
+		in.close();
+		return(contents);
+	}
+	throw(errno);
+}
+
 Shader::Shader(const char* vertexFile, const char* fragmentFile) {
-    this->id = this->createShaders(vertexFile, fragmentFile);
+    this->createShaders(vertexFile, fragmentFile);
     GLCall(glUseProgram(this->id));
 }
 
