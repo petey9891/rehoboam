b1c91a49afe263b758ea9568a467deee7665c6f4
Andrew Peterson <16apeterson@gmail.com>
2021-12-24 10:47:01 -0600

    chore: slow down animation

--- src/Applications/Christmas.cpp
+++ src/Applications/Christmas.cpp
@@ -11,31 +11,23 @@ void Christmas::setCommand(Command cmd) {
 }
 
 void Christmas::run() {
-    usleep(5 * 1000);
+    usleep(10 * 1000);
     this->continuum += 1;
     this->continuum %= 3 * 255;
 
     int r = 0, g = 0, b = 0;
 
-    printf("BEGINNING r: %d, g: %d, b: %d, continuum: %d\n", r, g, b, this->continuum);
     if (this->continuum <= 255) {
         int c = this->continuum;
-        printf("in FIRST section of continuum -- c: %d\n", c);
-        // b = 255 - c; // starts with blue first
         g = 255 - c; // starts with green first
         r = c;
     } else if (this->continuum > 255 && this->continuum <= 511) {
         int c = this->continuum - 256;
-        printf("in SECOND section of continuum -- c: %d\n", c);
         r = 255 - c;
         g = c;
     } else {
         this->continuum -= 512;
-        // printf("in THIRD section of continuum -- c: %d\n", c);
-        // g = 255 - c;
-        // b = c;
     }
-    printf("END r: %d, g: %d, b: %d, continuum: %d\n", r, g, b, this->continuum);
 
     this->canvas->Fill(r, g, b);
     this->canvas = this->matrix->SwapOnVSync(this->canvas);
